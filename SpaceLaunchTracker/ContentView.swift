8597bfc2e76e96983328a5a40b1033c3b3b8d730
Jacob Silvas <jacobsilvas47@gmail.com>
2026-05-31 09:58:17 -0700

    Initial Commit

---
+++ SpaceLaunchTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpaceLaunchTracker
+//
+//  Created by Jacob Silvas on 5/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpaceLaunchTracker/SpaceLaunchTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceLaunchTrackerApp.swift
+//  SpaceLaunchTracker
+//
+//  Created by Jacob Silvas on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct SpaceLaunchTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
